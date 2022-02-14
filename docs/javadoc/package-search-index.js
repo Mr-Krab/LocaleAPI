286803b75fbc12c92fe3fbaed33b9c8babe7eaa5
Иван <StalkerIv@yandex.ru>
2022-02-14 18:28:50 +0300

    Delete docs/javadoc directory

--- docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"sawfowl.localeapi"},{"l":"sawfowl.localeapi.api"},{"l":"sawfowl.localeapi.serializetools"},{"l":"sawfowl.localeapi.utils"}];updateSearchResults();
